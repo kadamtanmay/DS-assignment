316ffc5fd2ee5efc4243278e97a16a0ae70e1329
Tanmay Kadam <kadamtanmay2017@gmail.com>
2023-05-22 00:12:40 +0530

    Add files via upload

--- 1/AddClient.java
+++ 1/AddClient.java
@@ -1,18 +1,35 @@
 import java.rmi.*;
-public class AddClient {
-public static void main(String args[]) {
-try {
-String addServerURL = "rmi://" + args[0] + "/AddServer";
-AddServerIntf addServerIntf =
-(AddServerIntf)Naming.lookup(addServerURL);
-System.out.println("The first number is: " + args[1]);
-double d1 = Double.valueOf(args[1]).doubleValue();
-System.out.println("The second number is: " + args[2]);
-double d2 = Double.valueOf(args[2]).doubleValue();
-System.out.println("The sum is: " + addServerIntf.add(d1, d2));
-}
-catch(Exception e) {
-System.out.println("Exception: " + e);
-}
-}
+import java.util.Scanner;
+
+
+
+public class Client{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		try{
+			String serverURL = "rmi://localhost/Server";
+			ServerIntf serverIntf = (ServerIntf) Naming.lookup(serverURL);
+			
+			System.out.print("Enter First Number: ");
+			double num1 = sc.nextDouble();
+			
+			System.out.print("Enter Second Number: ");
+			double num2 = sc.nextDouble();
+			
+			
+			
+			
+			
+			System.out.println("--------------- Results ---------------");
+			System.out.println("Addition Is: " +serverIntf.Addition(num1, num2));
+		
+			
+			
+		}catch(Exception e){
+			System.out.println("Exception Occurred At Client!" + e.getMessage());
+		}
+		
+	}
+
 }
